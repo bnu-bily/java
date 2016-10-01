8576e5d891878f11bd0d6886d11c6971ad690e42
Administrator <Administrator@192.168.1.5>
2016-10-01 17:23:45 +0800

    修改了searchfactor   intparsebinary  searchspeacial

--- src/com/bily/fourth/IntParseBinary.java
+++ src/com/bily/fourth/IntParseBinary.java
@@ -1,11 +1,12 @@
 package com.bily.fourth;
 
+import java.util.Scanner;
+
 import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
 
 /**
- * 4.37
- * 编写程序，提示用户输入一个十进制整数，然后显示对应的二进制值。在这个程序中不要使用
- * Interger.toBinaryString(int)方法
+ * 4.37 编写程序，提示用户输入一个十进制整数，然后显示对应的二进制值。在这个程序中不要使用 Interger.toBinaryString(int)方法
+ * 
  * @author Administrator
  *
  */
@@ -13,14 +14,17 @@ public class IntParseBinary {
 
 	public static void main(String[] args) {
 		int testNum = 10;
+		System.out.println("Please input a num:");
+		Scanner input = new Scanner(System.in);
+		testNum = input.nextInt();
 		String result = "";
-		while(testNum != 0){
-			result+=(testNum % 2);
+		while (testNum != 0) {
+			result = (testNum % 2) + result;//modify
 			testNum /= 2;
 		}
-		
-		for (int i = (result.length()-1); 0 <= i; i--) {
-			System.out.print(result.substring(i,i+1)+" ");//使用字符串截取函数
+
+		for (int i = 0; i < result.length(); i++) {
+			System.out.print(result.charAt(i) + " ");// 使用字符串截取函数
 		}
 	}
 
--- src/com/bily/fourth/SearchFactor.java
+++ src/com/bily/fourth/SearchFactor.java
@@ -3,25 +3,28 @@ package com.bily.fourth;
 import java.util.Scanner;
 
 /**
- * 4.16
- * （找出一个整数的因子）编写程序，读入一个整数，然后以升序显示他的所有最小因子。
+ * 4.16 （找出一个整数的因子）编写程序，读入一个整数，然后以升序显示他的所有最小因子。
+ * 
  * @author Administrator
  *
  */
 public class SearchFactor {
 
 	public static void main(String[] args) {
-		int testNum = 120;//测试的数值
-		System.out.println("Please input a testNum(int)");
+		int testNum = 120;// 测试的数值
+		System.out.println("Please input a testNum(int) bigger than 1");
 		Scanner inpScanner = new Scanner(System.in);
 		testNum = inpScanner.nextInt();
-		while(testNum != 1){
-			for(int i = 2;i <= testNum;i++){
-				if (testNum % i == 0) {
-					System.out.print(i+" ");
-					testNum = testNum / i ;
-					break;
-				}
+
+		for (int i = 2; i <= testNum;) {//modify
+			if (testNum % i == 0) {
+				System.out.print(i + " ");
+				testNum = testNum / i;
+			} else if (testNum == 1){
+				break;
+			} else {
+				i++;
+			
 			}
 		}
 
