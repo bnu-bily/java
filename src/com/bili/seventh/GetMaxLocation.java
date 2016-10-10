6ac1689e4973012fd06f4474156e79c908f2ef53
Administrator <Administrator@192.168.1.10>
2016-10-10 17:01:18 +0800

    seventh

---
+++ src/com/bili/seventh/GetMaxLocation.java
@@ -0,0 +1,49 @@
+package com.bili.seventh;
+
+import java.util.Scanner;
+
+
+
+/**
+ * 7.13定位最大元素，提示用户输入一个二维数组，找到最大值的行列坐标，返回一维数组，数组存放最大值得行列坐标
+ * @author Administrator
+ *
+ */
+public class GetMaxLocation {
+
+	public static void main(String[] args) {
+		final int SIZE = 5;
+		System.out.println("请输入一个5行5列二维数组：");
+		double[][] array = new double[SIZE][SIZE];
+		Scanner input = new Scanner(System.in);
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				array[i][j] = input.nextDouble();
+			}
+		}
+		getMaxLocation(array);
+		int[] show = getMaxLocation(array);
+		System.out.println("最大值坐标为"+"<"+show[0]+","+show[1]+">");
+		
+	}
+	/**
+	 * 
+	 * @param array 
+	 * @return 数组第一个值代表行，第二代表列
+	 */
+	public static int[] getMaxLocation(double[][] array) {
+		int[] location = new int[2];
+		double max = 0.0;
+		max = array[0][0];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (array[i][j] > max) {
+					max = array[i][j];
+					location[0] = i;
+					location[1] = j;
+				}
+			}
+		}
+		return location;
+	}
+}
---
+++ src/com/bili/seventh/Inverse.java
@@ -0,0 +1,57 @@
+package com.bili.seventh;
+
+import java.util.Scanner;
+
+/**
+ * ¼ÆËã2x2¾ØÕóµÄÄæ¾ØÕó
+ *|a  b|            |d	-b| 
+ *|	   | = 1/(ad-bc)|	  |
+ *|c  d|			|-c	 a|
+ * @author Administrator
+ *
+ */
+public class Inverse {
+	final static int NUM = 2;
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("ÇëÊäÈëÒ»¸ö¶şÎ¬¾ØÕó£º");
+		double[][] test = new double[NUM][NUM];
+		for (int i = 0; i < test.length; i++) {
+			for (int j = 0; j < test[0].length; j++) {
+				test[i][j] = input.nextDouble();
+			}
+		}
+		double[][] show = inverse(test);
+		for (int i = 0; i < show.length; i++) {
+			for (int j = 0; j < show[0].length; j++) {
+				System.out.print(show[i][j] + "  ");
+			}
+			System.out.println("\n");
+		}
+	}
+	/**
+	 * 
+	 * @param A 2 * 2 ¾ØÕó
+	 * @return 2 * 2 ÄæÕó
+	 */
+	public static double[][] inverse(double[][] A) {
+		if (A.length == 2) {
+			if (A[0].length == 2) {
+				double temp = (A[0][0] * A[1][1]) - (A[0][1] * A[1][0]);
+				if (temp != 0) {
+					double[][] V = new double[NUM][NUM];
+					V[0][0] = (1 / temp) * A[1][1];
+					V[0][1] = (1 / temp) * A[0][1] * (-1);
+					V[1][0] = (1 / temp) * A[1][0] * (-1);
+					V[1][1] = (1 / temp) * A[0][0];
+					return V;				
+				}
+				return null;
+			}
+			return null;
+		} else {
+			return null;
+		}
+		
+	}
+}
